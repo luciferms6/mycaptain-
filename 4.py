e0aa4704aad92adde66829a26b3c19aa10dc7fbb
luciferms6 <67362323+luciferms6@users.noreply.github.com>
2020-06-24 21:30:36 +0530

    #3rd project work of mycaptain
    This file shows the output of printing all positive numbers in a range

---
+++ 4.py
@@ -0,0 +1,11 @@
+list1 = [12,-7,5,64,-14]
+for num in list1:
+    if num>=0:
+        print(num,end = " ")
+list2 = [12,14,-95,3]
+num = 0
+while(num < len(list2)):
+    if list2[num] >= 0:
+        print(list2[num], end = " ")
+    num += 1
+    
